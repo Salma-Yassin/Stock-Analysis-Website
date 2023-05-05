4340b6603c615320f409538a956fcb9d2a59a987
RadwaSawy <s-radwa.elsawy@zewailcity.edu.eg>
2023-05-05 21:03:07 +0300

    notification check

--- apps/static/assets/js/mainfile.js
+++ apps/static/assets/js/mainfile.js
@@ -1,7 +1,177 @@
-import { Mediator } from "../mediator.js";
+
+class Mediator {
+  constructor() {
+    this.handlers = {};
+  }
+
+  register(key, handler) {
+    if (!this.handlers[key]) {
+      this.handlers[key] = [];
+    }
+    this.handlers[key].push(handler);
+  }
+
+  unregister(key, handler) {
+    if (!this.handlers[key]) {
+      return;
+    }
+    const index = this.handlers[key].indexOf(handler);
+    if (index !== -1) {
+      this.handlers[key].splice(index, 1);
+    }
+  }
+
+  notify(key, data) {
+    if (!this.handlers[key]) {
+      return;
+    }
+    this.handlers[key].forEach(function (handler) {
+      handler(data);
+    });
+  }
+}
 
 // Create a new Mediator object
 const mediator = new Mediator();
+
+/*var getStockData = $.get('/main-dashboard-data');
+// Get the table body element
+ const tableBody = document.querySelector("tbody");
+ getStockData.done(function (stocks) {
+ const stockData = JSON.parse(stocks);
+ const tableBody = document.querySelector("tbody");*/
+mediator.register("dataAdded", function () {
+  window.location.href = "/watchlist.html";
+});
+
+mediator.register("get_chart_data", function (data) {
+  const tableBody = document.querySelector("#Dashboard_tbody");
+  //stockData = JSON.parse(stockData);
+  for (const [symbol, financialData] of Object.entries(data)) {
+    const row = document.createElement("tr");
+
+    const symbolCell = document.createElement("td");
+    symbolCell.textContent = symbol;
+    row.appendChild(symbolCell);
+
+    const currentPriceCell = document.createElement("td");
+    currentPriceCell.textContent =
+      financialData.financialData.currentPrice.fmt;
+    row.appendChild(currentPriceCell);
+
+    const targetHighPriceCell = document.createElement("td");
+    targetHighPriceCell.textContent =
+      financialData.financialData.targetHighPrice.fmt;
+    row.appendChild(targetHighPriceCell);
+
+    const targetLowPriceCell = document.createElement("td");
+    targetLowPriceCell.textContent =
+      financialData.financialData.targetLowPrice.fmt;
+    row.appendChild(targetLowPriceCell);
+
+    const targetMeanPriceCell = document.createElement("td");
+    targetMeanPriceCell.textContent =
+      financialData.financialData.targetMeanPrice.fmt;
+    row.appendChild(targetMeanPriceCell);
+
+    const targetMedianPriceCell = document.createElement("td");
+    targetMedianPriceCell.textContent =
+      financialData.financialData.targetMedianPrice.fmt;
+    row.appendChild(targetMedianPriceCell);
+
+    const recommendationCell = document.createElement("td");
+    recommendationCell.appendChild(
+      document.createTextNode(financialData.financialData.recommendationKey)
+    );
+    row.appendChild(recommendationCell);
+
+    const addButtonCell = document.createElement("td");
+    const addButton = document.createElement("button");
+    addButton.textContent = "Add";
+    addButton.classList.add("btn", "btn-block", "btn-info");
+    addButton.setAttribute("type", "button");
+    addButtonCell.appendChild(addButton);
+
+    addButton.addEventListener("click", function () {
+      const requestData = {
+        [symbol]: financialData,
+      };
+      console.log(financialData.financialData);
+      console.log(requestData);
+      $.ajax({
+        type: "POST",
+        url: "/add_to_watchlist",
+        data: JSON.stringify(requestData),
+        contentType: "application/json",
+        success: function (response) {
+          console.log(response);
+          // Notify the mediator that the data has been added
+          mediator.notify("dataAdded");
+        },
+      });
+    });
+
+    addButtonCell.appendChild(addButton);
+    row.appendChild(addButtonCell);
+
+    tableBody.appendChild(row);
+  }
+});
+
+$.get("/data", function (data) {
+  mediator.notify("get_chart_data", data);
+});
+
+$.get("/main-dashboard-news-data", function (data) {
+  mediator.notify("newsDataUpdated", data);
+});
+
+
+mediator.register("newsDataUpdated", function (newsData) {
+  // Get the table body element
+  //console.log(newsData)
+  newsData = JSON.parse(newsData);
+
+  for (const [symbol, newsItem] of Object.entries(newsData)) {
+    const cardId = `#${symbol}_news_card`; // Construct the card ID based on the current symbol
+    const card = document.querySelector(cardId);
+
+    // Update the card elements with data from the current newsItem
+    const cardImg = card.querySelector("img");
+    const cardTitle = card.querySelector("h2");
+    const cardText = card.querySelector("p");
+    const cardSource = card.querySelector(".card-footer span:first-child");
+    const cardTime = card.querySelector(".card-footer span:last-child");
+
+    cardImg.src = newsItem.banner_image;
+    cardTitle.textContent = newsItem.title;
+    cardText.textContent = newsItem.summary;
+    cardSource.textContent = newsItem.source;
+
+    const timeString = newsItem.time_published;
+    console.log(newsItem.time_published);
+    const date = new Date(
+      timeString.slice(0, 4),
+      Number(timeString.slice(4, 6)) - 1,
+      timeString.slice(6, 8),
+      timeString.slice(9, 11),
+      timeString.slice(11, 13),
+      timeString.slice(13, 15)
+    );
+
+    const formattedDate = date.toLocaleDateString("en-US", {
+      month: "long",
+      day: "numeric",
+      year: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+    });
+    console.log(formattedDate);
+    cardTime.textContent = formattedDate;
+  }
+});
+
 
 // Register a handler function with the mediator to update the data when it becomes available
 mediator.register("dataUpdated", function () {
@@ -34,7 +204,7 @@ mediator.register("dataRemoved", function () {
 
 mediator.register("getStockData", function (stockData) {
   //const stockData = JSON.parse(stocks);
-  const tableBody = document.querySelector("tbody");
+  const tableBody = document.querySelector("#watchlist_tbody");
   console.log(stockData);
 
   // Select the list element by its ID
@@ -466,3 +636,39 @@ const myChart = new Chart(ctx, {
                            }
                          }
                        })*/
+
+
+/* For Notification */
+
+// Register a handler function with the mediator to remove the corresponding list item when the data has been deleted
+mediator.register("dataDeleted", function (params) {
+  const button = params.button;
+  const itemId = params.itemId;
+  // Find the corresponding list item and remove it
+  const listItem = button.closest(".item");
+  listItem.remove();
+});
+
+const deleteButtons = document.querySelectorAll(".delete-button");
+console.log('delete buttons')
+console.log(deleteButtons)
+// Add a click event listener to each delete button
+deleteButtons.forEach((button) => {
+  
+  button.addEventListener("click", () => {
+    const itemId = button.dataset.id;
+
+    $.ajax({
+      type: "POST",
+      url: "/delete_notification",
+      data: JSON.stringify(itemId),
+      contentType: "application/json",
+      success: function (response) {
+        console.log(response);
+        // Notify the mediator that the data has been deleted
+        mediator.notify("dataDeleted", { button: button, itemId: itemId });
+      },
+    });
+  });
+});
+
