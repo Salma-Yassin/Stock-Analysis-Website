806fe4b1e004f0d7e512e4dae49f5c0a990d8e07
RadwaSawy <s-radwa.elsawy@zewailcity.edu.eg>
2023-05-23 20:33:05 +0300

    adding app insights key for production app

--- apps/config.py
+++ apps/config.py
@@ -1,14 +1,6 @@
-# -*- encoding: utf-8 -*-
-"""
-Copyright (c) 2019 - present AppSeed.us
-"""
-
 import os
 
 class Config(object):
-
-    # Application Insights configuration
-    APPINSIGHTS_INSTRUMENTATIONKEY = '3b0ae1d8-dc8f-4803-8faa-a53ee9d667bd'
 
     basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -19,3 +11,8 @@ class Config(object):
 
     # App Config - the minimal footprint
     SECRET_KEY = os.getenv('SECRET_KEY', 'S#perS3crEt_9999')
+
+    if DEBUG:  # Use testing Application Insights key in debug mode
+        APPINSIGHTS_INSTRUMENTATIONKEY = '3b0ae1d8-dc8f-4803-8faa-a53ee9d667bd'
+    else:  # Use production Application Insights key in non-debug mode
+        APPINSIGHTS_INSTRUMENTATIONKEY = '1eec00af-9938-4154-94b0-f11fd3993ead'
