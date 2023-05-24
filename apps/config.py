0fe4599ea0e42bba12e7223587605ed68c5a4291
RadwaSawy <s-radwa.elsawy@zewailcity.edu.eg>
2023-05-24 22:58:27 +0300

    insight keys

--- apps/config.py
+++ apps/config.py
@@ -12,7 +12,7 @@ class Config(object):
     # App Config - the minimal footprint
     SECRET_KEY = os.getenv('SECRET_KEY', 'S#perS3crEt_9999')
 
-    if DEBUG:  # Use production Application Insights key in debug mode
+    if DEBUG:  # Use testing Application Insights key in debug mode
+        APPINSIGHTS_INSTRUMENTATIONKEY = '3b0ae1d8-dc8f-4803-8faa-a53ee9d667bd' 
+    else:  # Use production Application Insights key in non-debug mode
         APPINSIGHTS_INSTRUMENTATIONKEY = '1eec00af-9938-4154-94b0-f11fd3993ead'
-    else:  # Use testing Application Insights key in non-debug mode
-        APPINSIGHTS_INSTRUMENTATIONKEY = '3b0ae1d8-dc8f-4803-8faa-a53ee9d667bd'
